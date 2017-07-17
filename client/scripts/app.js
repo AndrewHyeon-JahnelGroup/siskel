f48b967ccf44e182a2edbd6cc1e417c243c4c681
Hack Reactor Student <student@hackreactor.com>
2017-07-17 15:39:24 -0700

    finished siskel

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -5,7 +5,8 @@ var Movie = Backbone.Model.extend({
   },
 
   toggleLike: function() {
-    // your code here
+
+    this.set('like', !this.get('like'));
   }
 
 });
@@ -16,12 +17,16 @@ var Movies = Backbone.Collection.extend({
 
   initialize: function() {
     // your code here
+    this.on('change', this.sort, this);
   },
 
   comparator: 'title',
 
   sortByField: function(field) {
     // your code here
+    this.set('comparator', field);
+    this.comparator = field;
+    this.sort();
   }
 
 });
@@ -59,6 +64,8 @@ var MovieView = Backbone.View.extend({
 
   initialize: function() {
     // your code here
+    this.render();
+
   },
 
   events: {
@@ -67,6 +74,7 @@ var MovieView = Backbone.View.extend({
 
   handleClick: function() {
     // your code here
+    this.model.toggleLike();
   },
 
   render: function() {
@@ -80,6 +88,7 @@ var MoviesView = Backbone.View.extend({
 
   initialize: function() {
     // your code here
+    this.on('sort', this.render(), this);
   },
 
   render: function() {
